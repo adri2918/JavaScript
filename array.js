b87caebbcc0a5d50c5de4425a105637c9b1f0cb4
Adri <adri@tviso.com>
2018-12-12 16:08:23 +0100

    try(1) count vowels

--- array.js
+++ array.js
@@ -5,12 +5,30 @@ let words = [];
 for(let i = 0 ; i<phrases.length; i++){
     words.push(phrases[i].split(" ").length);   
 }
+
+
 // array.map
-count_words = phrases.map((function(element, index , array){// element (pasa el valor de la cada posicion de la array) index (es el número que representa la posición de element) array(el objeto entero)
-    let  wordss= []; // array que contendrá el numero de palabras de cada posición
-    wordss =  array[index].split(" ").length;
-    return wordss;
-}
-));
+count_words = phrases.map
+    (function(element, index , array){// element (pasa el valor de la cada posicion de la array) index (es el número que representa la posición de element) array(el objeto entero)
+        let  wordss= []; // array que contendrá el numero de palabras de cada posición
+        wordss =  array[index].split(" ").length;
+        return wordss;
+    } );
 
 console.log(count_words);
+
+
+// try vowels
+let count_vowels =  [];
+count_vowels = phrases.map(function (element,index,array)
+{let inm = 0;
+    let a = element.split("");
+    a.forEach(element => {
+       let i =  element.indexOf("aeiou");
+       if(i !==-1){
+           inm++;
+       }
+    });
+    return inm;
+});
+console.log(count_vowels);
