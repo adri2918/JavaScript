eed0e371feb7408583e9bd2683dbbebe4abf5982
Adri <adri@tviso.com>
2018-12-12 15:25:34 +0100

    add correct function map

--- array.js
+++ array.js
@@ -1,17 +1,16 @@
 const phrases = ["hola", "hola adri", "quiero una banana", "lorem ipsum dolor sit amet"];
-
+let count_words = [];
 let words = [];
 
 for(let i = 0 ; i<phrases.length; i++){
     words.push(phrases[i].split(" ").length);   
 }
-let rels;
+count_words = phrases.map((function(element, index , array){
+    let  wordss= [];
+    wordss =  array[index].split(" ").length;
+    return wordss;
+}
+));
 
- rels = phrases.map(function(element){
-    
-     return element.length;
- }
-);
-
-console.log(rels);
+console.log(count_words);
 // array.map
