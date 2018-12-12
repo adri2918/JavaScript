df2ccd111fb45f2411a5ae485019d2465aeea99d
Adri <adri@tviso.com>
2018-12-12 15:30:38 +0100

    comment at code

--- array.js
+++ array.js
@@ -5,12 +5,12 @@ let words = [];
 for(let i = 0 ; i<phrases.length; i++){
     words.push(phrases[i].split(" ").length);   
 }
-count_words = phrases.map((function(element, index , array){
-    let  wordss= [];
+// array.map
+count_words = phrases.map((function(element, index , array){// element (pasa el valor de la cada posicion de la array) index (es el número que representa la posición de element) array(el objeto entero)
+    let  wordss= []; // array que contendrá el numero de palabras de cada posición
     wordss =  array[index].split(" ").length;
     return wordss;
 }
 ));
 
 console.log(count_words);
-// array.map
