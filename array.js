d0dfe80e09bffc06d1d8386556f2e7ec85030c72
Adri <adri@tviso.com>
2018-12-13 16:23:41 +0100

    master class pau, count vowel working and anonymous functions

--- array.js
+++ array.js
@@ -1,30 +1,43 @@
 const phrases = ["hola", "hola adri", "quiero una banana", "lorem ipsum dolor sit amet"];
-let count_words = [];
-let words = [];
-
-for(let i = 0 ; i<phrases.length; i++){
-    words.push(phrases[i].split(" ").length);   
-}
+// let count_words = [];
 
 
-// array.map
-count_words = phrases.map
-    (function(element, index , array){// element (pasa el valor de la cada posicion de la array) index (es el número que representa la posición de element) array(el objeto entero)
-        let  wordss= []; // array que contendrá el numero de palabras de cada posición
-        wordss =  array[index].split(" ").length;
-        return wordss;
-    } );
 
-console.log(count_words);
+// function countWords(phrase){// element (pasa el valor de la cada posicion de la array) index (es el número que representa la posición de element) array(el objeto entero)
+//     if (phrase === undefined || phrase === '') {
+//         return 0
+//     }
+    
+//     return phrase.split(" ").length;
+// } 
 
 
-// try vowels
+// function countVowels(element)
+// {
+//     return element.split("").reduce((prev, char) => {
+//         if ("aeiou".indexOf(char) !==-1){
+//             return prev + 1
+//         }
+
+//         return prev
+//     }, 0);
+// }
+
+// console.log(countVowels("hola"))
+
+
+// // array.map
+// count_words = phrases.map(countVowels);
+
+// console.log(count_words);
+
+console.log(phrases.map(phrase => phrase.replace(/[^aeiou]/g, '').length))
 let count_vowels =  [];
 count_vowels = phrases.map(function (element,index,array)
 {let inm = 0;
     let a = element.split("");
     a.forEach(element => {
-       let i =  element.indexOf("aeiou");
+       let i =  "aeiou".indexOf(element);
        if(i !==-1){
            inm++;
        }
