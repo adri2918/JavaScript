c677749affdf9f54ec62a1e8e18400de9ffeea42
Adri <adri@tviso.com>
2018-12-18 16:18:29 +0100

    add exception

--- sortmovies.js
+++ sortmovies.js
@@ -22,36 +22,13 @@ const movies = [
         addedAt: "2012-01-30"
     }
 ]
-
-const myCallbacks = require('./moviesFunction')
-const movies = [
-    {
-        info: {
-            title: "Babel",
-            rating: 8
-        },
-        addedAt: "2011-12-08"
-    },
-    {
-        info: {
-            title: "Her",
-            rating: 9
-        },
-        addedAt: "2016-07-24"
-    },
-    {
-        info: {
-            title: "Trainspotting",
-            rating: 7
-        },
-        addedAt: "2012-01-30"
-    }
-]
 const stdin = process.openStdin()
-stdin.addListener("data", function(input) { 
-    console.log("you entered: [" + input.toString().trim() + "]")
-    input.toString().trim().toLowerCase();
-    console.log(`[${input}]`);
-    console.log(movies.sort(myCallbacks[input]))
+stdin.addListener("data", function(input) {
+    input = input.toString().trim().toLowerCase();
+    console.log("you entered: [" + input + "]")
+    if(myCallbacks[input] === undefined){
+        console.log("Error - function doesnt exist")
+    }else{
+        console.log(movies.sort(myCallbacks[input]))
+    }
 })
-
